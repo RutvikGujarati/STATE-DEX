480e28f2caddb6a3e176d6b5ad8d10da60edc44d
RutvikGujarati <mrrutvikgujarati0007@gmail.com>
2025-05-12 17:40:18 +0530

    removed ownership from state token

--- src/MainTokens/StateToken.sol
+++ src/MainTokens/StateToken.sol
@@ -2,9 +2,8 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract STATE_Token_V2_1_Ratio_Swapping is ERC20, Ownable(msg.sender) {
+contract STATE_Token_V2_1_Ratio_Swapping is ERC20 {
     uint256 public constant MAX_SUPPLY = 1000000000000000 ether; // 1 QUADRILLION
 
     constructor(
@@ -13,10 +12,7 @@ contract STATE_Token_V2_1_Ratio_Swapping is ERC20, Ownable(msg.sender) {
         address _five,
         address _swap
     ) ERC20(name, symbol) {
-        require(
-            _five != address(0) && _swap != address(0),
-            "Invalid address"
-        );
+        require(_five != address(0) && _swap != address(0), "Invalid address");
 
         uint256 Five_percent = (MAX_SUPPLY * 5) / 100;
         uint256 ninetyFivePercent = MAX_SUPPLY - Five_percent;
